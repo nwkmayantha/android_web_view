535b8811b01bc4e95c9b5ed910e98c8f5f7906a0
Kasun Mayantha <39111962+nwkmayantha@users.noreply.github.com>
2020-04-14 21:07:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/WorkAbleWebApp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.WorkAbleWebApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Hide Title Bar and Set Full Screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+
+        setContentView(R.layout.activity_main);
+
+        //WebView
+        mWebView = (WebView) findViewById(R.id.activity_main);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://workableproto-001-site1.htempurl.com/");
+        mWebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
